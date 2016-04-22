9d3debe93fc03b65f5ac112f709cca9d1ebb9b30
Filip Krska <filip.krska@pirati.cz>
2016-04-22 16:47:03 +0200

    Zahraly_houslicky C,G

---
+++ Raduza-Zahraly_houslicky.C
@@ -0,0 +1,52 @@
+Radůza - Zahrály housličky
+
+Orig: A - Kapo: 9
+
+Intro:
+
+  Cmaj             Fmaj        Cmaj            Fmaj
+𝅘𝅥𝅮 G     A   G  E   G           G    A  G  E    F  E  D
+
+Cmaj               Fmaj
+Zahrály housličky, zahrály tence,
+
+Cmaj                   Fmaj
+foukám kouř do svíčky, čekám na milence,
+
+Dmi7                C#maj
+ten někde toulá se, nebo šel domů,
+
+Cmaj              Fmaj            Cmaj         Fmaj
+zahrály housličky tence, Bůh ví komu
+𝅘𝅥𝅮                                    C  H  G   A    F  E  D
+            
+Cmaj              Fmaj
+Zahrály housličky v protějším domě,
+
+Cmaj                   Fmaj
+srkám čaj z přesličky, vždyť hrajou pro mě,
+
+Dmi7           C#maj
+barokní gavota přes dvůr se nese,
+
+Cmaj               Fmaj          Cmaj             Fmaj
+ať tak a nebo tak, lásky nebojme se
+𝅘𝅥𝅮                                G     A   G  E   F
+
+  Cmaj            Fmaj
+𝅘𝅥𝅮 G     C  H  G   A    F  E  D
+
+Cmaj              Fmaj
+Zahrály housličky po sté takt třetí,
+
+Cmaj                Fmaj
+jak pára z konvičky do nebe letí,
+
+Dmi7           C#maj
+Hynku, Viléme, Jarmilo, Jano,
+
+Cmaj                   Fmaj          Cmaj  Fmaj          Cmaj
+foukám kouř do svíčky, zkus to na piáno    zkus to na piáno
+
+Fmaj          Cmaj6
+zkus to na piáno
