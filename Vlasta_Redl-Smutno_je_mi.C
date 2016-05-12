4c502d3e5726c080d79211550b4d5c0a165fac2e
Filip Krska <filip.krska@pirati.cz>
2016-05-12 10:41:40 +0200

    various additions

---
+++ Vlasta_Redl-Smutno_je_mi.C
@@ -0,0 +1,53 @@
+Vlasta Redl - Smutno je mi
+
+https://www.youtube.com/watch?v=VH4LAm0kf8Y
+
+Orig: C
+
+I: 2x
+
+  C                                       Dmi                 G  Gadd4  G
+𝅘𝅥𝅮 ^C  E  F  G    ^C  G    F  F    E  E    D  E  F  A    D  C  H  C  D  ^G
+
+   C      Dmi    Emi    F      C             D7   G  Gadd4  G
+1. Smutno je mi, smutno je mi, na ten smutek není lék,
+
+   C      Dmi      Emi   F       C          G  C
+   slíbil jsem ti, že se změním, a skutek neu  tek'
+
+                  Fmaj       C  /H  Ami7  Dmi      G           C  Fmaj
+   Změnil jsem se od základu,             jaks' to chtěla, johoho,
+
+   C           Fmaj    Emi  A7sus4  A7  Dmi7  G          C
+   jenže to má jednu vadu  -            že už není pro koho.
+
+
+   C      Dmi    Emi    F      C           D7  G  Gadd4  G
+2. Smutno je mi, smutno je mi, a nikdo nic netuší,
+
+   C     Dmi    Emi   F    C             G  C
+   nikdo si na  týhle zemi pro mě slzy nesu ší,
+
+               Fmaj      C  /H  Ami7  Dmi  G          C  Fmaj 
+   je to pěkné naděle ní,             proměnit se docela,
+
+       C           Fmaj   Emi  A7sus4  A7  Dmi7         G            C
+   ale nejhorší je pomyšlení,              že už bys mě stejně nechtěla.
+
+I:
+
+   C      Dmi    Emi  F     C             D7   G  Gadd4  G
+3. Smutno je mi, moje milá, když tak píšu po nocích,
+
+   C    Dmi    Emi   F      C           G  C
+   a ta láska, ta mi zbyla, nemůžu si pomo ci,
+
+              Fmaj      C  /H  Ami7  Dmi   G           C  Fmaj 
+   co lidí se na mě dívá,            poslouchá a přemýšlí,
+
+        C           Fmaj    Emi  A7sus4  A7  Dmi7  G          C
+   a já tobě hraju, tobě zpívám,             a ty  dávno neslyšíš.
+
+   C      Dmi    Emi    F      C G C
+*: Smutno je mi, smutno je mi ...
+
