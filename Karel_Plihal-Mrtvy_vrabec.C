5972889f1860597b53649348ba6ca1e79131ecf7
Filip Krska <filip.krska@pirati.cz>
2016-05-24 11:47:28 +0200

    Monter_blues, Mrtvy_vrabec

--- Karel_Plihal-Mrtvy_vrabec.C
+++ Karel_Plihal-Mrtvy_vrabec.C
@@ -9,30 +9,40 @@ Takhle nějak to bylo http://www.karelplihal.cz/desky/tntb.php
 
     C            Edim   F           Fmi
 1. Jenom trocha sazí a trocha peří,
+
     C        Edim        F     D#dim
    na světě mi to nikdo neuvěří,
+
     C           C#9 C9   F            D#dim   C                      G#7 G7
    že tam ležel             na nároží          starý a mrtvý vrabec.
 
     C            Edim           F            D#dim
 2. O červnových třešních snad se mu zdálo,
+
     C                 Edim        F          D#dim
    když jsem nad ním stál, právě poprchalo,
+
     C             C#9 C9   F           D#dim  C                       C#9 C9
    a on tam ležel            na nároží          starý a mrtvý vrabec.
+
+R:
     F                         As7
-*: Zatím dole v dancingu tři snědí páni v smokingu
+   Zatím dole v dancingu tři snědí páni v smokingu
+
      C7             A7
    hrají pro cizí vkus,
+
      D7                         D#dim
    stesk jim voči zavírá, když kytarista zazpívá
+
         As          G  G+
    svý nejnovější blues.
 
     C            Edim   F           D#dim
 3. Jenom trocha sazí a trocha peří,
+
     C        Edim        F      Fmi
    na světě mi to nikdo neuvěří,
+
     C           C#9 C9   F            D#dim   C                     Cmaj
    že tam ležel             na nároží          starý a mrtvý vrabec.
-
