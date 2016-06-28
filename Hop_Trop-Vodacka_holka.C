542df7452d0942e73c93f6732fb749dfbcf51dfd
Filip Krska <filip.krska@pirati.cz>
2016-06-28 10:36:26 +0200

    many updates

---
+++ Hop_Trop-Vodacka_holka.C
@@ -0,0 +1,66 @@
+Samson a jeho parta - Vodácká holka
+
+https://www.youtube.com/watch?v=wm5C1e9WzQ4
+
+Orig: D - Kapo: 2
+
+I:
+        C       Dmi     F      G
+        C       Dmi     /F  G  C   /F  /E  /D    /C  /F  /E  /D
+
+
+        C       Dmi    F      G            C            Dmi      F  G
+1. Když vlny se valí a peřeje řvou       a šutry z nich civí jak sů vy,
+
+       C        Dmi    F         G           C          Dmi       G    C
+   loď praská a umrlčí zvon bije tmou,   tak řekni, kdo pomoct ti     umí
+
+       Ami        Emi      F        C        Ami      Emi        G  C
+*: Kdo pádlem jen kmitne a převrátí loď, pak ječí jak staženej z ků že,
+
+      Ami        Emi     F           C        D                    G  Gsus4  Gsus4  G
+   že ty jseš ta atrapa, ty jseš ten cvok, co za každý cvaknutí  může.
+
+
+      C         Dmi    F        G         C        Dmi    F  G
+2. Je pěkná jak ráno a voní jak les, když na dřevo u ohně hrává,
+
+      C          Dmi     F         G       C         Dmi      G   C
+   je zrádná jak kočka a věrná jak pes, ta holka, co při tobě  spává.
+
+R:
+      F        Ami      G      C     F         Ami  G  C
+   Tu vodáckou holku si navěky nech, netop ji, neza há něj,
+
+        Dmi        Ami       Emi      Ami
+   svou víru, svou sílu, svý štěstí i pech,
+
+       Dmi          C            D        G
+   svý strachy, svý prachy, svůj poslední dech
+
+      C       Dmi  /F  G      C       Dmi      /F  G  C  /F  /E  /D    /C  /F  /E  /D
+   jí poctivě odevzdá  vej    a lásku za lásku     dá vej.
+
+       C       Dmi     F       G             C           Dmi       F  G
+3. Kdo připálí rejži a přesolí čaj,      kdo sůl nechá v posledním kempu,
+
+       C         Dmi       F         G          C           Dmi      G     C
+   kdo ráj změní v peklo a peklo zas v ráj, kdo zná všechny písničky   trempů.
+
+
+       Ami      Emi      F       C         Ami         Emi         G   C
+*: Kdo přeje ti štěstí a kope ti hrob, kdo sladce hned šeptá, hned vříská,
+
+     Ami      Emi       F         C      D                      G  Gsus4  Gsus4  G
+   a koho bys roztrh' a praštil a kop' a po kom se celej den stýská.
+
+       C        Dmi     F        G      C          Dmi       F  G
+4. Až vlasy nám odbarví nebeskej král a vrásky nám počmáraj' ků ži,
+
+       C         Dmi    F           G      C        Dmi      G   C
+   svý holce dej pusu a řekni "jdem dál" a na pádlo polož jí   růži.
+
+R:
+... 
+      C       Dmi  /F  G      C       Dmi      /F   G  C
+   jí poctivě odevzdá  vej    a lásku za lásku   dá a  vej.
---
+++ Krestan-Tanecnice.C
@@ -0,0 +1,66 @@
+Druhá tráva - Tanečnice
+
+https://www.youtube.com/watch?v=HGypJfEf-8Q
+
+Orig: D - Kapo: 2
+
+I:
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G
+𝅘𝅥𝅮     C    H  A     G         G   G  A  vC  D  E  D
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G6    G
+𝅘𝅥𝅮     C    H  A     G         G   G  vG A   D  C  E  E  G  A  G  G  E  D
+
+   C          G          Fmaj                         C          Fmaj C
+1.   Převrhlá židle jako symbol času         v nočním baru a pak holé nic
+    C         G          Fmaj                    C           Fmaj   C
+jen v rohu si flitry vyčesává z vlasů               poslední z tanečnic
+
+
+        C        G               Fmaj                     C                Fmaj C
+2. Jsem poslední host s poslední sklenkou v dlani   i ten klobouk mám dnes naposled
+   C         G         Fmaj                               C           Fmaj C  C
+   dám si ho do týla a koukám na ni                   jak šminkou obelhává svět
+
+R:
+   C G F    C    Fmaj     C     Fmaj C   G       C G   F     C   Fmaj C  G     F    C
+   Josefíno zatanči    mi na rozloučenou      za tuhle noc nedopitou  a    nedokončenou
+
+I:
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G
+      Á-á-áá-á-áá ...
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G6    G
+      Á-á-áá-á-áá ...
+
+   C         G          Fmaj                          C            Fmaj C
+3.   Bere si tašku moje balerína             v ní jen klíč a láhev od piva
+   C         G           Fmaj                         C          Fmaj C
+     porovná židle a pak pozhasíná                  a potichu se usmí vá
+
+   C         G              Fmaj                      C         Fmaj   C
+4.   Otvírám dveře - brzkým rozedněním         se pak rozmařile nechám vést
+   C          G           Fmaj                        C         Fmaj     C    C
+     toho kdo zvítězí nad pokušením              čeká nebe plné jitřních hvězd
+
+R:
+   C G F    C    Fmaj     C     Fmaj C   G       C G   F     C   Fmaj C  G     F    C
+   Josefíno zatanči    mi na rozloučenou      za tuhle noc nedopitou  a    nedokončenou
+
+I:
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G
+      Á-á-áá-á-áá ...
+      Ami  G  Fmaj  Fmajsus2  C      Fmaj         G6    G
+      Á-á-áá-á-áá ...
+
+   C         G         Fmaj                           C          Fmaj C
+5.   Severní vítr metá svoje kouzla na mém            vlnolamu v Tortuze
+   C         G           Fmaj                         C        Fmaj C
+     do mého nitra právě tiše vklouzla jedna          polonahá ilu  ze
+
+   C            G          Fmaj                       C         Fmaj     C
+6.   Kdesi v mé duši tančí tanečnice            možná odvážněji než bych chtěl
+   C        G           Fmaj                          C          Fmaj C  C
+     odráží od břehů má plachetnice                 a říkejte mi Izma el
+
+R: 2x
+
+I: fade...
