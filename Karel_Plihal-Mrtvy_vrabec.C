e78fefda33a99eba468196fc55d0bb032b37526d
Filip Krska <filip.krska@pirati.cz>
2016-06-30 10:12:54 +0200

    many updates

--- Karel_Plihal-Mrtvy_vrabec.C
+++ Karel_Plihal-Mrtvy_vrabec.C
@@ -1,48 +1,46 @@
 Karel Plíhal - Mrtvý vrabec
 
-mostly according to: https://www.youtube.com/watch?v=t2QofMwdex0
-but refrain according to the track from album
+Mostly according to https://www.youtube.com/watch?v=t2QofMwdex0
+Refrain according to the track from album
 Takhle nějak to bylo http://www.karelplihal.cz/desky/tntb.php
 
+Orig: C
 
    C Edim F D#dim
 
-    C            Edim   F           Fmi
-1. Jenom trocha sazí a trocha peří,
-
-    C        Edim        F     D#dim
+   C                 Edim     F           Fmi
+1. Jenom trocha sazí        a trocha peří,
+   C        Edim        F       D#dim
    na světě mi to nikdo neuvěří,
+   C                C#9 C9   F            D#dim
+      že tam ležel             na nároží
+   C    (silent)          G#7 G7
+      starý a mrtvý vrabec.
 
-    C           C#9 C9   F            D#dim   C                      G#7 G7
-   že tam ležel             na nároží          starý a mrtvý vrabec.
-
-    C            Edim           F            D#dim
-2. O červnových třešních snad se mu zdálo,
-
-    C                 Edim        F          D#dim
+   C                    Edim      F           D#dim
+2. O červnových třešních     snad se mu zdálo,
+   C                 Edim        F          D#dim
    když jsem nad ním stál, právě poprchalo,
-
-    C             C#9 C9   F           D#dim  C                       C#9 C9
-   a on tam ležel            na nároží          starý a mrtvý vrabec.
+   C                 C#9 C9   F           D#dim
+      a on tam ležel             na nároží
+   C        (silent)       C#9 C9
+      starý a mrtvý vrabec.
 
 R:
-    F                         As7
+   F                         As7
    Zatím dole v dancingu tři snědí páni v smokingu
-
-     C7             A7
+   C7             A7
    hrají pro cizí vkus,
-
-     D7                         D#dim
+   D7                          D#dim
    stesk jim voči zavírá, když kytarista zazpívá
-
-        As          G  G+
+       As         G     G+
    svý nejnovější blues.
 
-    C            Edim   F           D#dim
-3. Jenom trocha sazí a trocha peří,
-
-    C        Edim        F      Fmi
+   C                 Edim    F           D#dim
+3. Jenom trocha sazí       a trocha peří,
+   C        Edim        F      Fmi
    na světě mi to nikdo neuvěří,
-
-    C           C#9 C9   F            D#dim   C                     Cmaj
-   že tam ležel             na nároží          starý a mrtvý vrabec.
+   C               C#9 C9   F             D#dim
+      že tam ležel               na nároží
+   C      (silent)         Cmaj
+      starý a mrtvý vrabec.
