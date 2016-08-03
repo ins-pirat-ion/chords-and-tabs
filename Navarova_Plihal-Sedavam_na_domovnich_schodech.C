39f9f84ef2b65b51cbc12753b3f9976527cc603a
Filip Krska <filip.krska@pirati.cz>
2016-08-03 15:47:18 +0200

    schody, huslicky

---
+++ Navarova_Plihal-Sedavam_na_domovnich_schodech.C
@@ -0,0 +1,62 @@
+Josef Kainar, Karel Plíhal, Zuzana Navarová - Sedávám na domovních schodech
+
+https://www.youtube.com/watch?v=GEM1ImKQvoY
+
+Orig: B - Kapo 10 :)
+
+Dmi7:        x-x-0-2-1-1        x-5-7-5-6-5 (in solo)
+D9:          x-x-0-2-1-0        x-5-3-5-5-5 (in chorus)
+B7:          x-1-0-1-x-x
+E7:          0-7-6-7-5-0
+E7/6:        x-7-x-7-9-9
+Ami7:        5-7-5-5-5-5
+Ami7/6add9:  5-7-5-5-7-7
+D7:          x-5-4-5-3-x
+G+:          x-x-x-4-4-3        x-x-x-8-8-7
+A+:          x-x-x-6-6-5
+Ami:         x-x-x-5-5-5   (in solo)
+F9:          x-8-7-8-8-x
+C6:          x-3-5-5-5-5
+G9:          x-10-9-10-10-10
+
+   Cmaj Edim      Dmi7         G7         Cmaj Edim  Dmi7              G7
+1. Sedá vám       na domovních schodech   zpívávám   v krásných cizích slovech
+   C7     H7    B7       A7               D9       G6     Cmaj  G7
+   kterým jenom sama rozumím              z komínu stoupá dým
+
+   Cmaj  Edim   Dmi7        G7           Cmaj  Edim  Dmi7             G7
+2. Stíny jdou   stíny nebem táhnou       stíny jdou  do mých strun mi sáhnou
+   C7      H7     B7      A7             D9       G6     Cmaj  /D  /D#
+   ke všem písním zapomenutým            z komínu stoupá dým
+
+R:
+   E7                         E7/6                      Ami7 E7       Ami7 /H /A
+   Všechna hnízda šla už spát je nutno myslet na návrat večerní zvony znějí
+   D7                   D9          D7      G7          /A /A /G G+
+   Zvolna nota za notou kráčí svojí samotou jako černou zá vě jí
+
+   Cmaj Edim  Dmi7        G7              Cmaj Edim  Dmi7     G7
+3. Sedá vám   svou kytaru v klíně         zpívávám   pomalu a líně
+   C7   H7       B7           A7          D9       G6     Cmaj
+   neznámá blues hlasem zastřeným         z komínu stoupá dým
+
+Solo:
+      C7 C#7 D7 D#7 E7    A+ Ami    Dmi7    F9     A7   A+ Ami   Dmi7 A7 Dmi7
+      C    Ami7   Dmi7   G6   C9      A7                Fmaj  G6/7  Cmaj  /D  /D#
+
+R:
+   E7                         E7/6                      Ami7 E7       Ami7 /H /A
+   Všechna hnízda šla už spát je nutno myslet na návrat večerní zvony znějí
+   D7                   D9          D7      G7          /A /A /G G+
+   Zvolna nota za notou kráčí svojí samotou jako černou zá vě jí
+
+   Cmaj Edim  Dmi7        G7              Cmaj Edim  Dmi7     G7
+3. Sedá vám   svou kytaru v klíně         zpívávám   pomalu a líně
+   C7   H7       B7           A7
+   neznámá blues hlasem zastřeným
+   Dmi7     G7     Cmaj     C6 H6 B6 A6
+   z komínu stoupá dým
+   Dmi7     G7     Cmaj     G9 F#9 F9 E9
+   z komínu stoupá dým
+   Fmaj     G6/7   Cmaj
+   z komínu stoupá dým
