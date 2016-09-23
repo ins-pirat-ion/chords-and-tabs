1065f543bf03236366462146ada9f7a618eed7fe
Filip Krska <filip.krska@pirati.cz>
2016-09-23 13:04:18 +0200

    lots

---
+++ Neckar-Chci_te_libat.C
@@ -0,0 +1,47 @@
+Václav Neckář - Chci Tě Líbat
+
+https://www.youtube.com/watch?v=kXvIGBTK_Wk
+
+            C                             G7
+1. Chci tě líbat, přijď lásko má, nač se skrývat,
+                E7         Ami G
+   přijď, už se nám začlo stmívat, já jsem tu sám den co den.
+
+2. Chci se dívat jak lehce v chůzi se houpeš,
+   ten pohled není snad loupež vem jaro sem do mých stěn.
+   
+   Ami                              G
+R. Nejíš, nespíš, nevíš jak dát mi zprávu,
+         Ami                     G
+   když telefon mám venku na nároží,
+           C              Ami         Dmi
+   stejně doufám, že zas přiletíš jak vítr,
+        Dmi7                  G7             C
+   jak příval co smýval prach dnů, dlouhých dnů.
+   
+3. Chci tě líbat, přijď lásko má, nač se skrývat,
+   přijď, už se nám začlo stmívat víc, a já jsem tu sám, den co den.
+   
+R. U vás nikdo nezvedá, náš dům zamkli,
+   padesátník poslední zrádně spad',
+   stejně doufám, že zas přiletíš jak vítr,
+   jak příval co smýval prach dnů, dlouhých dnů. 
+   Chci tě líbat
+   
+4. Máš snad křídla a v motýlech prý ses mi zhlídla,
+   to vím, že's mi víc nenabídla, a já bych tě chtěl den co den.
+  
+            C                      E7 Emi       Ami
+   Chci tě líbat, přijď lásko 1á - á jen u mne své místo máš,
+                                G C
+   buď sluncem všech mých čtyř stěn, čekám s vírou i dál, čekám s vírou i dál,
+          Emi                  E      E7
+   zvonek zní jak zdáli, ústa tvá mě pálí,
+         Ami                           G
+   už tě líbám, nebo aspoň mám krásný sen.
+
+            C                             G7
+1. Chci tě líbat, přijď lásko má, nač se skrývat,
+                E7         Ami G
+   přijď, už se nám začlo stmívat, já jsem tu sám den co den.
+
---
+++ Samson-Laciny_vino.C
@@ -0,0 +1,62 @@
+Samson - Laciný víno
+
+https://www.youtube.com/watch?v=DlJSclro4Fc
+
+I:
+   C     /H   /A  ???   G
+
+   C     /H   /A  ???   G
+
+   C                Emi         Ami
+1. Po noci krátké a po milování zbylo mi málo,
+   D7                     C     G7              C  (G7)
+   jen hlava v dlaních co bolí, to je to laciný víno
+
+   C            Emi             Ami                D7
+2. Po noci plné cigaret kouře byls jako tajfun a já jako bouře
+      C     G7              C
+   To bolí, to je to laciný víno.
+
+R1:
+   E7                                 Ami
+   Teď jenom matně v hlavě mi svítá, útržky večera vytanou
+   E7                                Dmi                F   G7
+   Tak jako stébla se tonoucí chytá, hledám tvou adresu napsanou
+
+    C                 Emi          Ami
+3.  Napsals ji sirkou vypálenou na krabičku cigaret,
+    D7          C       G7             C
+    Zpola plnou k ránu. Pilo se laciný víno.
+
+R2:
+   C      Emi  Ami D7          G
+   Večery fajnový u láhve litrový
+   C         Emi Ami   D7            G
+   se milují namátkou, holky co málo ví.
+   E7               Ami       D7                 G7
+   Když hlava potom bolí ráno spát se chce, je nedospáno,
+   C      Emi  Ami         D7   G7              C
+   Večery fajnový tak končívaj, to je to laciný víno.
+
+I:
+
+   C                Emi         Ami
+1* Po noci krátké a po milování zbylo jí málo,
+   D7                     C     G7              C  (G7)
+   Jen hlava v dlaních co bolí, to je to laciný víno
+
+R1:
+   E7                                 Ami
+   Teď jenom matně v hlavě mi svítá, útržky z večera vytanou
+   E7                                Dmi                F   G7
+   Tak jako stébla se tonoucí chytá, hledám tvou adresu napsanou
+
+   C            Emi             Ami             D7
+4. Adresu někdo vynesl s košem, tak vidíš holka rázem je po všem
+      C     G7              C
+   To bolí, to je to laciný víno.
+
+R2:
+
+O:
+
