5558b69ed945e42f22a4bd907943d38e5a629384
Filip Krska <filip.krska@pirati.cz>
2016-10-15 13:57:51 +0200

    lots of

---
+++ Suchy_Slitr-Praminek_vlasu.C
@@ -0,0 +1,68 @@
+Jiří Suchý - Pramínek vlasů
+
+https://www.youtube.com/watch?v=zOX299csGu8
+
+Orig: E - Kapo: 4
+
+I:
+𝅘𝅥𝅮  [: bass  C    H A   G F  F F# G   G  A  H  C   :]
+   [:   C      Ami   Dmi     G6  G6  F#6 F6   :]
+𝅘𝅥𝅮  whistle                       G   F   E
+   C     Ami        Dmi7 F6 F#6 G6   G6  F#6 F6 C     Ami        Dmi7 F6 F#6 G6   G6  F#6 F6
+𝅘𝅥𝅮  C          C D E G              G G   A   H  C          A G E D                G   F   E
+   C     Ami        Dmi7 F6 F#6 G6   G6  F#6 F6 C          F7          C   Dmi7
+𝅘𝅥𝅮  C          C D E G              G G   A   H  C  C  C    D# D#   D   C
+
+   G7         C       Ami7            Dmi7   F6 F#6  G6
+1. Když měsíc rozlije      světlo své po kraji
+   G6 F#6 F6 C      Ami               Dmi7  F6 F#6 G6
+   a  hvězdy řeknou,    že čas je jít spát,
+   G6 F#6 F6  C     Ami             Dmi7   F6 F#6 G6
+   pramí  nek vlasů     jí ustřihnu potají,
+   G6 F#6  F6 C         F7         C    G7
+   ko mu - no přece té, kterou mám rád.
+
+   G7       C     Ami7             Dmi7   F6 F#6  G6
+2. Pramínek vlasů      jí ustřihnu potají,
+   G6 F#6 F6  C           Ami             Dmi7  F6 F#6 G6
+   já blá zen pod polštář      chci si ho dát,
+   G6 F#6 F6  C         Ami         Dmi7     F6 F#6 G6
+   ač ko  liv sny se mi     zásadně nezdají,
+   G6 F#6  F6 C           F7       C6
+   vě řím, že dnes v noci budou se zdát.
+ 
+R:  
+            B        B A G# C                           B          B A G#  C
+   O sny mě připraví teprve svítání,         zpěv ptáků v oblacích a modré nebe,
+             Ami7/5b               C                  G#7               G76
+   od vlasů, jichž jsem se dotýkal ve spaní, nový den nůžkama odstřihne tebe.
+
+   G7         C        Ami7            Dmi7   F6 F#6  G6
+4. A na bílém polštáři,     do kroužku stočený,
+   G6 F#6 F6 C       Ami          Dmi7  F6 F#6 G6
+   zbude  tu po tobě     pramínek vlasů,
+   G6 F#6 F6  C        Ami             Dmi7     F6 F#6 G6
+já ne bu  du  vstávat,  dál chci ležet zasněný,
+   G6 F#6 F6  C        F7       C6  G76
+   je to  tiž neděle a mám dost času,
+
+Solo: 2x    trumpet, jazzy tune
+   [:3x  C   Ami        Dmi7 F6 F#6 G6   G6  F#6 F6  :] C          F7          C   Dmi7
+𝅘𝅥𝅮  G A H C  ....
+
+R:  
+            B        B A G# C                           B          B A G#  C
+   O sny mě připraví teprve svítání,         zpěv ptáků v oblacích a modré nebe,
+             Ami7/5b               C                  G#7               G76
+   od vlasů, jichž jsem se dotýkal ve spaní, nový den nůžkama odstřihne tebe.
+
+   G7         C        Ami7            Dmi7   F6 F#6  G6
+4. A na bílém polštáři,     do kroužku stočený,
+   G6 F#6 F6 C       Ami          Dmi7  F6 F#6 G6
+   zbude  tu po tobě     pramínek vlasů,
+   G6 F#6 F6  C        Ami             Dmi7     F6 F#6 G6
+já ne bu  du  vstávat,  dál chci ležet zasněný,
+   G6 F#6 F6  C      F76        C6  F76               C      F76          C6  F76
+   je to  tiž neděle   a mám dost času,      je totiž neděle   a mám dost času,
+            C      F76          C6  Ami7 Dmi7  G6 F#6 F6  Cmaj
+   je totiž neděle   a mám dost času,
