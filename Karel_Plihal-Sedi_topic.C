5cb68d3c77573018a13cbbb5b55410ae7de4afca
Filip Krska <filip.krska@pirati.cz>
2016-10-31 16:39:38 +0100

    many things

---
+++ Karel_Plihal-Sedi_topic.C
@@ -0,0 +1,26 @@
+Karel Plíhal - Sedí topič u piána
+
+https://youtu.be/Qekr1Tl-a0Q
+
+
+        C                   Emi
+1. Sedí topič u piána, preluduje potají
+         G                    C      D9/F# G
+   a pod prsty toho pána bílé klapky černa jí.
+          C
+
+         C                     Emi
+2. Za ta léta u divadla, která strávil v kotelně,
+         G                            C    F7    C
+   hraje svoje "Pec nám spadla" stále více fortelně.
+
+R:
+   C9                   F
+   Zatímco se hudebníci vybavují u kávy,
+   D7                      G           G#  G
+   topič si nad klávesnicí    vyhrnuje rukávy.
+
+1.
+
+4. Slaví, celý rozesmátý, konec topné sezóny
+   a kysličník uhelnatý rozložil se na tóny.
