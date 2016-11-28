458178d95ded197674de86ec9a0be708a43b8e68
Filip Krska <filip.krska@pirati.cz>
2016-11-28 19:02:36 +0100

    Chci_te_libat Dva

--- Neckar-Chci_te_libat.C
+++ Neckar-Chci_te_libat.C
@@ -2,48 +2,57 @@ Václav Neckář - Chci Tě Líbat
 
 https://www.youtube.com/watch?v=kXvIGBTK_Wk
 
-Orig: A
+Orig: A - Kapo: 9
 
-            C                             G7
+I:
+      C       C7        F      Ddim          C     Ami        G       G7
+♪  G  E     D C   E F G F   A  D    F  F E D G   A E    E D C D   G E G
+
+           C                             G        Gsus4
 1. Chci tě líbat, přijď lásko má, nač se skrývat,
-                E7         Ami G
-   přijď, už se nám začlo stmívat, já jsem tu sám den co den.
+   G            Ddim      Ami         /F#        D7         G
+   přijď, už se nám začlo stmívat, a  já jsem tu sám den co den.
 
+           C                          G        Gsus4
 2. Chci se dívat jak lehce v chůzi se houpeš,
-   ten pohled není snad loupež vem jaro sem do mých stěn.
-   
-   Ami                              G
-R. Nejíš, nespíš, nevíš jak dát mi zprávu,
-         Ami                     G
-   když telefon mám venku na nároží,
-           C              Ami         Dmi
-   stejně doufám, že zas přiletíš jak vítr,
-        Dmi7                  G7             C
-   jak příval co smýval prach dnů, dlouhých dnů.
-   
+   G          Ddim      Ami    /F#      D7             G
+   ten pohled není snad loupež vem jaro sem do    mých stěn.
+
+R1:
+   Ami                             G            Ami                      G
+   Nejíš, nespíš, nevíš jak dát mi zprávu, když telefon mám venku na nároží,
+            C              Ami7         Dmi7
+   stejně   doufám, že zas přiletíš jak vítr,
+       Dmi7/G G6     Dmi7/G G6        Dmi7/G G6       Dmi7/G
+   jak pří    val co smý    val prach dnů,   dlouhých dnů.
+
+           C                             G        Gsus4
 3. Chci tě líbat, přijď lásko má, nač se skrývat,
+   G            Ddim      Ami            /F#        D7          G
    přijď, už se nám začlo stmívat víc, a já jsem tu sám, den co den.
-   
-R. U vás nikdo nezvedá, náš dům zamkli,
-   padesátník poslední zrádně spad',
+
+R2:
+   Ami                          G       Ami                        G
+   U vás nikdo nezvedá, náš dům zamkli, padesátník poslední zrádně spad',
+          C              Ami7         Dmi7
    stejně doufám, že zas přiletíš jak vítr,
-   jak příval co smýval prach dnů, dlouhých dnů. 
-   Chci tě líbat
-   
+       Dmi7/G G6     Dmi7/G G6        Dmi7/G G6       Dmi7/G        C
+   jak pří    val co smý    val prach dnů,   dlouhých dnů.  Chci tě líbat
+
+I:
+          C7            F      Dmi7/5-       C         Ami        G          G7
+♪  E F G  C     E F G v A      F    F F E D  G    ^E   A     ^E   D   D C H  G
+
+            C          (silence)           G        Gsus4
 4. Máš snad křídla a v motýlech prý ses mi zhlídla,
+   G            Ddim       Ami      /F#        D7           G
    to vím, že's mi víc nenabídla, a já bych tě chtěl den co den.
-  
-            C                      E7 Emi       Ami
-   Chci tě líbat, přijď lásko 1á - á jen u mne své místo máš,
-                                G C
-   buď sluncem všech mých čtyř stěn, čekám s vírou i dál, čekám s vírou i dál,
-          Emi                  E      E7
+
+           C                  G            Ddim      E7        Ami
+*. Chci tě líbat, přijď lásko máááááááááá  jen u mne své místo máš,
+       Ami Ami7      Ami Ami7     G           C
+   buď sluncem všech mý  ých čtyř stěn, čekám s vírou i dál, čekám s vírou i dál,
+          G                   Ddim     E7
    zvonek zní jak zdáli, ústa tvá mě pálí,
-         Ami                           G
-   už tě líbám, nebo aspoň mám krásný sen.
-
-            C                             G7
-1. Chci tě líbat, přijď lásko má, nač se skrývat,
-                E7         Ami G
-   přijď, už se nám začlo stmívat, já jsem tu sám den co den.
-
+           Ami               Ami Ami7    Ami Ami7 G            C
+   Ooo tak už tě líbám, nebo as  poň mám krá ásný sen. Chci tě líbat (fade 1.)
