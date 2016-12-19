9468b59b3d60847e2d5fe728e4b714554848cb1e
Filip Krska <filip.krska@pirati.cz>
2016-12-19 16:58:42 +0100

    Byla_cesta, Vanocni_pisen

---
+++ Karel_Plihal_Jaroslav_Seifert-Vanocni_pisen.C
@@ -0,0 +1,46 @@
+Karel Plíhal - Vánoční píseň
+
+https://youtu.be/HlSKDuGEI-s
+
+Orig: D - Kapo 2
+
+I:
+   Ami7   D   G   C   Dmi7  G6  C  Cadd2  C   Cadd2  C
+
+       C         Emi         Dmi7         Gadd4  G
+1. Jak ptáci zimomřiví v tom teskném příběhu,
+       Ami7     Emi        Fmaj7         Gadd4  G
+   ti, které neuživí třpyt hvězdic na sněhu,
+      Ami7               Emi     F          G E7
+   my sešli jsme se snící    nad lžičkou olova,
+       Ami7   Dmi7  G    C      Dmi7 G    C   Cadd2  C
+   kéž by nám mohlo říci    náš osud doslova.
+
+        C          Emi          Dmi7     Gadd4  G
+2. Když už se řítí v spěchu ten žaluplný děj,
+      Ami7      Emi        Fmaj7       Gadd4  G
+   svíčičko na ořechu, hoř pro nás pomalej,
+      Ami7         Emi      F         G  E7
+   ať zvučí trouby hlásné i zpěvy andělů dřív,
+       Ami7   Dmi7   G       C        Dmi7    G   C   /C /H
+   než přijde smrt a zhasne  náš dech ve svém pocelu.
+
+I:
+   Ami7   D   G   C   Dmi7  G6  C  Cadd2  C
+
+     C           Emi       Dmi7       Gadd4  G
+3. A těm u sukní matek dej úsměv do oček,
+        Ami7     Emi     Fmaj7     Gadd4  G
+   klín plný cukrlátek a hodně vánoček,
+        Ami7       Emi          F        G  E7
+   když už se řítí v spěchu ten žaluplný děj,
+      Ami7  Dmi7  G       C     Dmi7      G   C   /C /H
+   svíčičko   na ořechu,    hoř pro nás   pomalej.
+      Ami7  Dmi7  G       C     Dmi7      G   C  Cadd2  C
+   svíčičko   na ořechu,    hoř pro nás   pomalej.
+
+O: 2x
+        C           Emi       Dmi7      Gadd4  G
+        Ami7        Emi       Fmaj7     Gadd4  G
+        Ami7        Emi       F         G  E7
+        Ami7  D     G    C    Dmi7  G   C  ( Cadd2  C )
