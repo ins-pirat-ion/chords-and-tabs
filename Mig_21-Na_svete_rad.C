bbb74e70896b216722474735f6ee09266dbe01c8
Filip Krska <filip.krska@pirati.cz>
2017-01-05 16:26:57 +0100

    Behind_Blue_Eyes, Na_svete_rad

---
+++ Mig_21-Na_svete_rad.C
@@ -0,0 +1,55 @@
+MIG 21 - Na světě rád
+
+Orig: C - Kapo 0
+
+I:
+   (8.fret)  F9  F9     D7    C     G#7 G7      G#7 G7
+
+              C          G7        C   C7          F     E7         Ami
+1. Když se mě zeptáš kdo myslím že jsem   popravdě řeknu že vlastně nevím
+            G#7      C         G#7        C     G7
+   Potom se ale zamyslím a pak rád ti odpovím
+
+           C        G7        C    C7          F        E7        Ami
+2. Po ránu vstanu a večer jdu spát   v noci se vyspím a s úsměvem vstávám
+          G#7          C       G#7      C
+   Uléhám taktéž s úsměvem takový už já jsem
+
+R:
+         C         /H        /A   /G   D7               Ami
+   Už to vím-už to vím-už to mám       písničku svou si zpívám
+         C         /H        /A   /G   D7         Ami
+   Už to vím-už to vím-už to mám       nikomu nezazlívám
+      D7            C                    C          G7       C     G7
+   Že nic mi nechce dát                a proto jsem na světě rád
+
+I:
+   C   G7  C  C7  F  E7  Ami7     G#7   C      C  G7  C  G7
+
+          C       G7     C    C7        F        E7         Ami
+3. Miluji květy i děti i zvěř   odpadky třídím a z trávy je sbírám
+          G#7          C       G#7      C   G7
+   šetřím vodou kypřím zem takový už já jsem
+
+            C       G7     C    C7             F       E7       Ami
+4. Žiju ten okamžik co právě je       o nic mi nejde a na ničem nelpím
+          G#7                C        G#7      C
+   Bojuji s pýchou, s egem i zlem takový už já jsem
+
+R:
+         C         /H        /A   /G   D7               Ami
+   Už to vím-už to vím-už to mám       písničku svou si zpívám
+         C         /H        /A   /G   D7         Ami
+   Už to vím-už to vím-už to mám       nikomu nezazlívám
+      D7            C                    C          G7       C     G7
+   Že nic mi nechce dát                a proto jsem na světě rád
+
+I:
+   C  /H   /A  /G      D7       Ami
+   C  /H   /A  /G      D7       Ami
+   G#7     C           C   G7   C
+
+   (slowing)  C          G7        C (pause,whisper)  F        E7      Ami
+5. Když se mě zeptáš kdo myslím že jsem      popravdě řeknu že vlastně nevím
+         G#7          C   G#7    C          G7       C  G7  C   C#9  C9
+   Nevím nevím nechci lhát     a proto jsem na světě rád        ou   jéééé
