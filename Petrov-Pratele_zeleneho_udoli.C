1a51db450af63be1da91035119f655fc4ed32234
Filip Krska <filip.krska@pirati.cz>
2017-01-27 14:26:44 +0100

    Klaun_a_tanecnice, Pratele_zeleneho_udoli

---
+++ Petrov-Pratele_zeleneho_udoli.C
@@ -0,0 +1,52 @@
+Jaroslava Wykrent, Vadim Petrov - Přátelé Zeleného údolí
+
+                                 https://youtu.be/JxB-Uw_S-lk
+Orig: D - Kapo: 2
+
+E7   Ami                       Dmi7    /G                Cmaj
+1. (rec.)   Znáš nebe bez mraků      a mraky zase bez nebe
+Ami          Dmi7        /G             Cmaj /G /C /H C7
+   Stříbrnou vodu řek, a zázraky jménem les
+C7           Dmi7  /G              C /C /C /H Ami
+   Dík křiku sojky poznáš jednou i sebe
+Ami         Dmi7          /G             C /G /C /H C7
+   Už zítra znát se budeš mnohem líp než dnes
+C7           Dmi7  /G              C /C /C /H Ami
+   Dík křiku sojky poznám jednou i sebe
+      Ami         Dmi7         /G             E    Dmi7  E  (drums)
+         Už zítra znát se budu mnohem líp než dnes
+R:
+   /G /A  /G  C                     G      C
+   Kdybys tak náhodou    měl pocit, že jsi sám
+              Emi                     G7     C
+   Tou cestou mechovou   přijď rychle rovnou k nám
+   C7         F              G7        Emi
+   Vždyť fůru přátel máš     v zeleném údolí
+         Ami          Dmi7                /Fmaj  /G   C   C7	
+            no tak si jich jen važ,    ať už jsi kdokoliv.
+I:
+    F      G      Emi      Ami      Dmi7      /F /G  C
+
+E7  Ami           Dmi7         /G          Cmaj
+2.     Pro štěstí do kapsy tři sojčí pírka dej si
+Ami         Dmi7        /G             Cmaj /G /C /H C7
+   A špetku jehličí, ať vždycky trefíš zpět
+C7           Dmi7     /G             C /C /C /H Ami
+   Kdybys to nevěděl, tak dávno malý nejsi
+Ami             Dmi7         /G         C /G /C /H C7
+   Vždyť v kůře stromů dá se najít celý svět
+C7            Dmi7     /G        C /C /C /H Ami
+   Kdybych to nevěděl, tak dávno malý nejsem
+      Ami             Dmi7         /G         E    Dmi7  E  (drums)
+         Vždyť v kůře stromů dá se najít celý svět
+R:
+   /G /A  /G  C                  G      C
+   Kdybys tak náhodou měl pocit, že jsi sám 
+              Emi                   G7     C   
+   Tou cestou mechovou přijď rychle rovnou k nám 
+   C7         F          G7        Emi
+   Vždyť fůru přátel máš v zeleném údolí
+         Ami          Dmi7             /Fmaj  /G   C   C7     
+            no tak si jich jen važ, ať už jsi kdokoliv.
+I:
+    F      G      Emi      Ami      Dmi7      /F /G  C
