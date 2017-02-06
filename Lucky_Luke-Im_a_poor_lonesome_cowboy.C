46de8c25f207cb5e3858f23fd27b8f402a0d8195
Filip Krska <filip.krska@pirati.cz>
2017-02-06 17:01:34 +0100

    Im_a_poor_lonesome_cowboy

---
+++ Lucky_Luke-Im_a_poor_lonesome_cowboy.C
@@ -0,0 +1,66 @@
+Lucky Luke - Poor lonesome cowboy
+                                    https://youtu.be/1EpfwaSPBB8
+          Orig: D - Kapo: 3
+R:
+      C                    /C  /D  /E  F                     /F /E  /D /C
+   We are  lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7       G7
+   You're a long long way from home
+      C                    /C  /D  /E  F                     /F /E  /D /C
+   We are  lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7  G7 C
+   You've a long long way to roam
+
+   C
+1. I'm a poor lonesome cowboy I'm a long long way from home
+                          Emi              Dmi7 Emi7 Fmaj D9/F#  G    /F /E /D
+   And this poor lonesome cowboy has got a long long way  to     roam
+        C                  C7      C9       F                F7
+   Over mountains and over prairies    from dawn 'til day is done
+         G7 Ami               Emi      F      G7      C    /G /F /E  /D  /C
+         My horse and me keep ridin' into the settin' sun  pa pa dam tam tam
+R:
+   C               /C  /D  /E  F                     /F /E  /D /C
+   Lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7       G7   /G  /F /E  /D  /C
+   You're a long long way from home ram pa dam tam tam
+   C               /C  /D  /E  F                     /F /E  /D /C
+   Lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7  G7 C    G  C
+   You've a long long way to roam
+
+   C
+2. There are guys who just figure every problem with a gun
+                       Emi            Dmi7 Emi7   Fmaj D9/F#  G    /F /E /D
+   And a finger on the trigger can be dan  gerous hurt some   one
+       C                   C7      D9      F                F7
+   But problems solve much better       by keeping calm and cool
+         G7 Ami               Emi      F       G7     C    /G /F /E  /D  /C
+         My horse and me keep ridin' I ain't nobody's fool pa pa dam tam tam
+R:
+   C               /C  /D  /E  F                     /F /E  /D /C
+   Lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7       G7   /G  /F /E  /D  /C
+   You're a long long way from home ram pa dam tam tam
+   C               /C  /D  /E  F                     /F /E  /D /C
+   Lonesome cowboy dam dam dam dam   lonesome cowboy da dam ta dam
+   C        Ami  Ami7 D7  G7 C
+   You've a long long way to roam
+
+   C
+3. I'm a poor lonesome cowboy but it doesn't bother me
+                             Emi              Dmi7  Emi7 Fmaj D9/F#  G    /F /E /D
+   'Cause this poor lonesome cowboy prefers a horse for  com  pa     ny
+       C               C7      C9         F                 F7
+   Got nothing against women        but I wave them all goodbye
+         G7 Ami               Emi       F          G7    C    /G /F /E  /D  /C
+         My horse and me keep riding we don't like being tied
+R:
+   C               Cmaj C7 C6  F
+   Lonesome cowboy                   lonesome cowboy
+   C        Ami  Ami7 D7       G7
+   You're a long long way from home
+   C               Cmaj C7 C6  F
+   Lonesome cowboy                   lonesome cowboy
+   C        Ami  Ami7 D7  G7 C            F          C  Ami7  C7     Cmi  G#7  C
+   You've a long long way to roam      to roam   wou ou
