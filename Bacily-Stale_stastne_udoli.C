18606559b2cc75cb2eb2834105c989bce84a9612
Filip Krska <filip.krska@pirati.cz>
2017-02-15 15:02:51 +0100

    Bacily-Stale_stastne_udoli.C

---
+++ Bacily-Stale_stastne_udoli.C
@@ -0,0 +1,53 @@
+Václav Neckář - Stále šťastné údolí
+                                          https://youtu.be/lshPrSxyh5M
+Orig: C
+
+   C                         Dmi7  Emi        Ami7          Fmaj  F
+1. Ty jsi byla stále šťastné údolí tobě pořád zářil v očích smích
+   G        E     E7   Ami    /G   Fmaj                    Gadd4  G
+   já věděl že mé rány přebolí     jak jen se dotknu dlaní tvých
+
+   C                          Dmi7     Emi       Ami7        Fmaj  F
+2. Když tě viděl táta řek´ mi vzápětí: ta se umí vážně hezky smát
+      G         E    E7   Ami    /G Fmaj     G           C  Dmi7  C Gadd2
+      ten úsměv vypůjčený od dětí   mě vítal v týdnu sedmkrát
+R:
+   G            F           C            Dmi7  C Gadd2
+   Když jsem tě neviděl pak dlouhou řadu dní
+   G            F          C      Dmi7  C Gadd2
+   žil jsem jak trosečník,   nic víc
+   G          F               C           Ami
+   pak přišel lístek, pozdrav poštou polední
+     Dmi7            Emi                         Fmaj       F
+   a já už nemoh dál větrem jsem se stal jenž ti letí říct:
+
+   C                         Dmi7  Emi         Ami7           Fmaj  F
+3. Ty jsi moje stále šťastné údolí do něhož se z dálek vracím rád
+G          E    E7   Ami    /G Fmaj         G             C  Dmi7 C Gadd2
+tvůj úsměv léčí mě i chlácholí, v něm vidím důvod proč se smát
+
+R2:
+   G          F            C               Dmi7  C Gadd2
+   Teď máš už vrásek pár a ztrácíš dětskou tvář
+   G         F             C  Dmi7  C Gadd2
+   o kalných vodách víš už víc
+   G           F            C             Ami
+   však přesto schováváš si úsměv pod polštář
+             Dmi7                Emi
+           a ten jak sladkou číš dál mi nabízíš
+R3:
+   G          F            C         Dmi7  C Gadd2
+   Teď máš už vrásek pár a táta není živ
+   G          F          C  Dmi7  C Gadd2
+   kdo za něj ocení tvou líc?
+   G            F              C           Ami
+   vždyť ty jak léta jdou, jsi stejná jako dřív
+     Dmi7                Emi                         Fmaj       F
+   i já jsem stejně mlád větrem chci se stát jenž ti letí říct :
+
+   C                        Dmi7      Emi      Ami7        Fmaj  F
+4. Vedle tebe skálu v písek rozdrolím zapomenu trápit se i bát
+        G          E     E7       Ami    /G Fmaj      G              C
+        jen zůstaň stále šťastným údolím    kam já se z dálek vracím rád
+O:
+    Dmi7    C    Fmaj   C
