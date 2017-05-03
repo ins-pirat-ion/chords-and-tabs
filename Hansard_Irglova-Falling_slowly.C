326a77a7816508bf65bb35e92354b31f24c9c852
Filip Krska <filip.krska@pirati.cz>
2017-05-03 20:40:12 +0200

    Falling_slowly

---
+++ Hansard_Irglova-Falling_slowly.C
@@ -0,0 +1,47 @@
+Glen Hansard and Marketa Irglova - Falling Slowly
+
+                                     https://youtu.be/FkFB8f8bzbY
+      Orig: C - Kapo: 0/5B
+I:
+   C   F   C   F
+
+   C                F              C                    F
+1. I don't know you but I want you all the more for that
+   C                     F                C                F
+   Words fall through me n'always fool me and I can't react
+   Ami        G      Fmaj     G                 Ami
+   Games that never amount to more than they're meant
+             G               F
+        Will play themselves out
+R:
+   C                 Fmaj F6            Ami7                 G   F
+   Take this sinking boat and  point it home we've still got time
+   C                  Fmaj  F6          Ami7                  G  F
+   Raise your hopeful voice you  have a choice You've made it now
+
+   C               F                 C                  F
+2. Falling slowly, eyes that know me and I can't go back
+   C                  F            C                    F
+   Moods that take me and erase me and I'm painted black
+   Ami      G         Fmaj      G               Ami
+   You have suffered enough and warred with yourself
+             G             F
+        It's time that you won
+R:
+   C                 Fmaj F6            Ami7                 G   F
+   Take this sinking boat and  point it home we've still got time
+   C                  Fmaj  F6          Ami7                  G  F
+   Raise your hopeful voice you  have a choice You've made it now
+I:
+        Ami   G   Fmaj   G   Ami   G   F          Cmaj
+                                       oooo  yeah ooo
+R:
+   C                 Fmaj F6            Ami7                 G   F
+   Take this sinking boat and  point it home we've still got time
+   C                  Fmaj  F6          Ami7                  G  F
+   Raise your hopeful voice you  have a choice You've made it now
+   C              Fmaj F6       Ami7            G     F
+   Falling slowly sing your melody I'll sing it long
+O:
+   C   Fmaj F6   Ami7     Fmaj             Cadd4    C  F  C  F  C
+                                  oooooo
