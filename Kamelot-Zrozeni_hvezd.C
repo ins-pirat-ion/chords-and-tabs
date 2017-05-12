1b759c1717eb8b79614df5c8dae98ddcef2476ce
Filip Krska <filip.krska@pirati.cz>
2017-05-12 15:32:43 +0200

    Kamelot-Zrozeni_hvezd

---
+++ Kamelot-Zrozeni_hvezd.C
@@ -0,0 +1,52 @@
+Kamelot - Zrození hvězd
+                                          https://youtu.be/jnkpuz73Bl0
+           Orig: A - Kapo 5/0B
+I:
+   C    Fadd2 F C       Fadd2  F Fmaj F
+
+   C                 F                Ami               G
+1. Zrození hvězd čas vykřesá kvapem a můžou mě vézt, až vyryju drápem
+         F            Ami              Gsus4  G
+   staré medvědice do skály větrů tvůj obličej.
+
+        C             F                   Ami        /B Emi
+2. Kraj otěhotněl jak prvnička v máji tou padlou tmou   a někde ve stáji
+          F             Ami                    Gsus4  G
+       se splaší koně a přiklušou blíž, ke mně blíž.     A
+R:
+   F                             Ami       G
+   březí klisna má oči jako ty a sedlo z něhy,
+         F                                Ami      G
+   možná se jí stýská, možná je zraněná a možná je ty,
+               F                                   C
+       všechno možná, všechno možná bude lepší než dřív.
+I:
+    Fmaj F  Ami7    G  Gsus2 G
+    F   Fmaj F F6   C                     F   Fmaj F F6  Ami  G   F  G
+
+           C            F              Ami            Emi
+3. Tahleta létavice mně do uší zpívá píseň panice, co z tváře ti slíbá
+        F             Ami                  Gsus4  G
+   tvůj vodopád slz a měsíční prach, co ty víš.
+
+        C          F              Ami            Emi
+4. Kraj Indiánů mě úsvitem vítá a ranní déšť mou rytinu smývá,
+           F                Ami                Gsus4  G
+       jsi vzdálená víc, co můžu ti říct, můžu říct,      jen to, že
+R:
+   F                             Ami       G
+   březí klisna má oči jako ty a sedlo z něhy,
+         F                                Ami      G
+   možná se jí stýská, možná je zraněná a možná je ty,
+               F                                   C
+       všechno možná, všechno možná bude lepší než dřív.
+I:
+       Fmaj F  Ami7    G  Gsus2 G
+       F   Fmaj F F6   C                   F   Fmaj F F6  Ami  G   F  G
+R:
+     F                             Ami       G
+   A březí klisna má oči jako ty a sedlo z něhy,
+         F                                Ami      G
+   možná se jí stýská, možná je zraněná a možná je ty,
+           F                                   C
+   všechno možná, všechno možná bude lepší než dřív.
