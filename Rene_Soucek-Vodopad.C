95a46a13e4bb56174e6375188da83d2bd28b2a35
Filip Krska <filip.krska@pirati.cz>
2017-05-12 15:33:34 +0200

    Rene_Soucek-Vodopad

---
+++ Rene_Soucek-Vodopad.C
@@ -0,0 +1,54 @@
+René Souček - Vodopád
+                       https://youtu.be/_GIRK3sZULY
+Orig: E - Kapo 4/8B
+
+I:
+      C                    Emi             F               Ami   F      G
+
+   C                    Emi             F               Ami   F      G
+1. Víš, až budu vstávat ráno, sny budou zamčený, pomalu svítá na obzoru
+   C                Emi           F             Ami    F          G
+   Nechám zas rozestláno, a znova píšu zlomenou tužkou z Koh-i-nooru
+R:
+      Ami                                 C
+   Že byla jsi nesmírně divoká, jak jarní řeka
+   Ami                                    C
+   Ten proud mě strhával s sebou, jak vodopád
+   Ami                                       C
+   a pramen, v němž jsme se topili už nepřitéká
+   G                   Ami                            C
+   Dávno už nemá spád, voda v něm zůstala stát
+
+   C                    Emi               F
+2. Zdáš se mi stále tak živá, ve snech se vracíš
+           Ami     F       G
+   a já tě z hlavy vymazat chtěl
+      C               Emi             F               Ami    F     G
+      Snídám antidepresiva, chvíli se ztrácíš, ale já stejně nezapomněl
+R:
+      Ami                                 C
+   Že byla jsi nesmírně divoká, jak jarní řeka
+   Ami                                    C
+   Ten proud mě strhával s sebou, jak vodopád
+   Ami                                       C
+   a pramen, v němž jsme se topili už nepřitéká
+   G                   Ami                            C
+   Dávno už nemá spád, voda v něm zůstala stát
+
+   C                       Emi            F               Ami   F       G
+3. Kdo ví, komu dáváš svou něhu mlčky bez nadějí, já naši láhev dopíjím sám
+   C               Emi                       F
+   Každý půjdem ke svému břehu, teď připíjím lásce,
+          Ami          F     G
+      jen škoda, že in memoriam
+R:
+      Ami                                 C
+   Že byla jsi nesmírně divoká, jak jarní řeka
+   Ami                                    C
+   Ten proud mě strhával s sebou, jak vodopád
+   Ami                                       C
+   a pramen, v němž jsme se topili už nepřitéká
+   G                   Ami                            C
+   Dávno už nemá spád, voda v něm zůstala stát
+O:
+      C                    Emi             F               Ami   F      C
