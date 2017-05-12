64c45043e5b37cf5157d269d3da39e55f166ade4
Filip Krska <filip.krska@pirati.cz>
2017-05-12 15:34:10 +0200

    Matuska-Tereza

---
+++ Matuska-Tereza.C
@@ -0,0 +1,39 @@
+Waldemar Matuška - Tereza
+                            http://youtu.be/5NfJijz5crQ
+Orig: C# - Kapo 1
+
+I:
+   C        Ami      Dmi7     G7  G  
+
+   C     Ami       Dmi   G7        C      C+    F D#dim
+1. Tereza,  jedině Tereza  měla by slyšet píseň mou,
+           C        C+          Ami      C+
+   ostatní prominou,   nestojím o jinou,
+              C                 D#dim  Dmi7     G7
+   jen pro tu jedinou chci tady žít a  zpívat.  Hmmmm
+
+   C     Ami       Dmi   G7        C      C+   F  D#dim
+2. Tereza,  jedině Tereza  je toho pravou příčinou,
+           C        C+         Ami      C+
+   že tady statečně,    hrdě a zbytečně
+            C      G7    C     H7
+   do větru zpívám píseň mou.  mmmm
+
+R:
+   G                      C     A7                  Dmi
+   Možná za rok za dva že pozná kdo měl její úsměvy rád
+   D7                   Ami7
+   láska že je sladká a hrozná,
+                   D9         G#7          G7
+   když se dostaví nečekaně a jednostranně.
+
+   C     Ami          Dmi   G7       C      C+      F
+3. Terezo,  přečti si Terezo  otázku kterou v očích mám
+   D#dim     C        C+           Ami      C+
+   s nápisem tiskacím    k tobě se obracím
+            C       G7     C
+   a ty víš předem, nač se ptám.
+
+O:
+                 Ami              F    G       C   F  C
+   Lááálalalalalaláááá  lalalalalalááá lááálalalááá
