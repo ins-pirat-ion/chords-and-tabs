29388f1727c4a2ab94f8cc3e1959b7972fc814db
Filip Krska <filip.krska@pirati.cz>
2017-05-29 09:27:25 +0200

    Do_ptacich_ploutvi_a_do_rybich_kridel

---
+++ Miki_Ryvola-Do_ptacich_ploutvi_a_do_rybich_kridel.C
@@ -0,0 +1,53 @@
+Miki Ryvola - Do ptačích ploutví a do rybích křídel
+
+Orig: Eb - Kapo:3
+
+I:
+   Cmaj        Fmaj     Dmi7 Ami7 Dmi7   F#/G#    F#/G# G#/B
+
+Cmaj     Ami7          Fmaj  Cmi75- Dmi75-
+1. Tak      snad abych šel,  sleč   no,
+Cmaj       Ami7    Fmaj        E7
+   krá     sný lži    den mění ve skutečno,
+Ami7    G  F  E7 Hmi75-  Ami7 Edim   D7                  Dmi7
+   mý   vlaky    chytneš stěží,      peříčka ptačí mi na cestu sněží,
+   C#maj                     Cmaj         Fmaj
+   bláznivý zvony mi cinkají z věží.
+
+Cmaj     Ami7           Fmaj  Cmi75- Dmi75-
+2. Teď         buď rozumná    sleč   na,
+Cmaj    Ami7  Fmaj         E7
+   krám   zavírám, a ty mi budeš vděčná,
+   Ami7 G F  E7 Hmi75- Ami7 Edim   D7                 Dmi7
+   svou roli    dohrál blá  zen,   nahoru do mraků vylítne rázem,
+             C#maj                    Cmaj 
+             tvrdě a mizerně přistává na zem.
+R:
+   Cmaj                  Cmi7   Hmi7
+   Lhůta záruční po ránu končí,
+   D#dim Hmi75- G#maj    B7      Cmaj
+   ze slibů až  pak    voprejská lak,
+Fmaj           E7       Emi    Ami7      Hmi75-  Ddim Cmaj 
+   letět do mraků s bláznem se ztratit a víc     se nevrátit,
+   D7               Dmi7
+   trapnej dotazník si píšeme,
+                C#maj               Cmaj      Fmaj   Cmaj    Fmaj
+             co se nehodí, to škrtneme.
+I: 2x
+   Cmaj     Ami7          Fmaj  Cmi75- Dmi75-
+   Cmaj     Ami7          Fmaj     E7
+   Ami7  G  F  E7  Hmi75-  Ami7  Edim
+   D7       Dmi7       C#maj    
+
+   Cmaj     Ami7          Fmaj  Cmi75- Dmi75-
+3. Tak         snad abych šel,  slečno,
+Cmaj   Ami7  Fmaj       E7
+   tvý přítulno se mění na netečno,
+Ami7  G  F   E7  Hmi75- Ami7  Edim  D7                  Dmi7
+   lásky jsem    dobral příděl,     zbytek si uschovám, co bych se styděl,
+             C#maj                          Cmaj         Fmaj Fmaj Fmi
+             do ptačích ploutví a do rybích křídel ...
+O:
+   Cmaj       Fmaj     Fmaj Fdim
+   Cmaj       Fmaj     Fmaj B7
+   Cmaj       Fmaj     Ami7 Gmi6
