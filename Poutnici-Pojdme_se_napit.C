917d00501caeb04ec82db3d217f22d6990b541f4
Filip Krska <filip.krska@pirati.cz>
2017-06-14 21:29:35 +0200

    Poutnici-Pojdme_se_napit

---
+++ Poutnici-Pojdme_se_napit.C
@@ -0,0 +1,46 @@
+Poutníci - Pojďme se napít
+                           https://youtu.be/pObv1UnQ1_M
+Orig: D - Kapo: 2
+
+                   F  G  C              Emi     F   C     G F Emi
+1. Pěkně tě vítám, lásko má, tak trochu zbitá a víc soukromá,
+   Dmi7     C        F           Emi            Ami
+   pěkně tě vítám, a čemu vděčím za tak vzácnou chvíli
+          F     Emi Dmi C      G    F  Emi
+          jednou        za sto let?
+
+R:
+    G     C                F   Emi Dmi  C     Dmi      F     C  C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
+                           F   Emi Dmi  C     Dmi      F     C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
+
+                     F Emi7 C               Emi     F   C    G F
+2. V nohách ti dřepí bílej  pták, už nejsme slepí a zlí, naopak,
+Emi   Dmi7      C       F            Emi          Ami
+      už nejsme slepí a rozdejchaní, jako když se spěchá
+          F  Emi Dmi C  G    F   Emi
+          bránou ví  tězství.
+
+R:
+    G     C                F   Emi Dmi  C     Dmi      F     C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
+                           F   Emi Dmi  C     Dmi      F     C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
+
+I:
+   C/Cmi              F   Emi Dmi7
+   C                  F   Emi Dmi7
+
+              C       F  G   C           Emi       F   C     G  F
+3. V očích ti svítí a sládne dech a něco k pití tu ční na stolech
+Emi   Dmi7     C       F            Emi        Ami
+      a něco k pití, a nepospíchej, stoletá má lásko,
+          F     Emi Dmi C   G   F  Emi
+          když už   ne  máš kam.
+
+R:
+    Dmi7  C                F   Emi Dmi  C     Dmi      F     C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
+                           F   Emi Dmi  C     Dmi      F     C
+Pojďme se napít, pojďme se napít,    ať nám mají z čeho slzy týct
