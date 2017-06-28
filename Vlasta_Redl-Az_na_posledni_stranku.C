707a7a5cc8dc079c2a89b89002213dd6484bffb2
Filip Krska <filip.krska@pirati.cz>
2017-06-28 13:16:33 +0200

    Letiste Az_na_posledni_stranku

---
+++ Vlasta_Redl-Az_na_posledni_stranku.C
@@ -0,0 +1,43 @@
+Vlasta Redl - Až na poslední stránku
+                                https://youtu.be/n7C2QCChfGE
+Orig: D - Kapo: 2
+
+F        Cadd4    C            Fmi           C
+1. Až na poslední stránku chci napsat to slůvko,
+         Cadd4     C      /H                 Dmi7
+   co mi svítí jak slunce   do mých mlhavých dní,
+         Cadd4    C           Ddim        C
+   na tu poslední stránku, co počmárat zbývá,
+                   Emi   Dmi7    C
+   kolem nakreslím kvítí    nebo sníh.
+
+         Cadd4    C            Fmi           C
+2. Na té poslední stránce bude jen jedno slůvko,
+       Cadd4     C      /H         Dmi7  Badd2
+   mým nejtěžším písmem    z první A,
+             Cadd4      C          Dmi75-        C
+   nač jinak všechna ta hejna dětí učit psát krásně,
+                        Emi     Dmi7    C
+             kdybych to nezvlád'    ani já.
+I:
+G9      C      Dmi75-   C      Cadd4   C   /E  Dmi7  Dmi75-
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+Cadd4   C      Ddim     C        Emi   Dmi7   C
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+   Cadd4  C            Dmi75-    C
+3. Zvolna chápu, proč slova se píšou,
+       Cadd4   C      /H             Dmi7
+   aby bylo co zpívat a aby bylo čím lhát,
+G9                     C          Ddim           C
+   všechna slova svých písní bych napsal tím slůvkem,
+                              Emi     Dmi7     C
+              tak chápeš, jak spěchám    se ho vzdát
+I:
+G9      C      Dmi75-   C        Cadd4   C   /E  Dmi7  
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+G9      C      Fmi      C           Emi   Dmi7    C
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+                    Emi     Dmi7    C
+    tak chápeš, jak spěchám   se ho vzdát
