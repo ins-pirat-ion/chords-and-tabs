30953e95e43ff773d85f7050fd955a8f2f5d5bf8
Filip Krska <filip.krska@pirati.cz>
2017-08-08 11:30:19 +0200

    Slunecny hrob, Ja a dym, Vlastovka

---
+++ Vlasta_Redl-Vlastovka.C
@@ -0,0 +1,61 @@
+Vlasta Redl - Vlaštovka
+                          https://youtu.be/hVqP73DQBjI
+Orig: G# - Kapo: 8
+
+I:
+   [:     C        /H        /A        /H       C         /H  /A /H  :]
+
+          C        /H        /A        /H       C         /H  /A /H
+1. Jednou večer mi do pokoje přiletěla papírová vlaštovka,       to mám
+   C                 /H                  /A            /H                 C      /H /A
+   z toho, že v zimě větrám, tak jsem ji zved a dal ji do tepla, zábla ji výškovka,
+/H             G#maj                Gmi7            G#maj                  D7sus4
+   pak jsem si prohlíd' její křídla jemně, proč asi přilétla, a proč právě ke mně,
+          C         /H     /A         /H       C      /H  /A
+   byla z Ahoj na sobotu a byla na ní otištěná hitovka.
+
+R:
+   /G   /A /H   C                            G         Dsus4  Eadd4 E
+   Nej  hezčí   dárek nemusí přinést vždycky Děda Mráz,
+               Ami   Ami7        D                     Gadd4
+   vletí k vám oknem něco, co by létat snad raději nemělo,
+            Bmaj                             Eadd4 E
+   někdo si vzpomněl, říkáte si, někdo na mě myslí,
+              Ami    Gadd4       F       Fadd2
+   nebo je to zas          jenom špás,
+          C         /H             /A    /H             C   /H  /A /H
+   tak či tak, když přiletí vlaštovka,   hned je v domě veselo.
+
+(voice_octave_up) C           /H       /A           /H        C        /H  /A /H
+2. Tak jsem si    pomalu slabikoval tu písničku, co přiletěla z éteru,        poslední
+   C              /H              /A           /H            C       /H  /A /H
+   rok jsem tolik skládal, že moc nechybělo, a složil bych i operu,
+             G#maj                  Gmi7      G#maj              D7sus4
+   jen místo múzy mě častěji líbala můra, ono písní se dá složit fůra,
+          C          /H               /A      /H      C        /H  /A /H
+   ale ne každá může letět, i když se vypustí správně po směru.
+
+             F     Fadd4      F         Fadd4       F         Fadd4  F      Fadd4
+*: K čemu to bylo, všechno to skládání, když stejně nikdo nic nepo   chopil
+          Dmi       Gmi7    Dmi7      C          F        Fadd4 F
+   a moje vlaštovky odlétly na jih, a s těma, co zůstaly, někdo topil,
+       Dmi7     Gmi7     Dmi7      C         F         Fadd4     F
+   jen jedna se vrátila, a já jsem čekal jak blázen, a věřil, že platí,
+      Dmi         Gmi7       Dmi7  C         F      Fadd4  F     C
+   že všechno, co necháš, ať letí, jednou se k tobě určitě vrátí,
+      Dmi7                   B               F      C     Gmi7
+   že všechno, co necháš, ať letí, jednou se k tobě možná vrátí.
+
+              C                  /H        /A    /H          C         /H  /A /H
+3. A tak jsem pálil archív svých písniček,       to se roura styděla,
+             C             /H       /A              /H           C       /H  /A /H
+   a když mi zbyl poslední papír,   hned jsem věděl     co s ním udělám,
+       G#maj               Gmi7          G#maj                                 D7sus4
+   roh na roh - šlo to bez problémů, jen jestli poletí, z toho měl jsem trochu trému,
+   C           /H           /A    /H           C        /H  /A  /H
+   to ví přece každý, že co letí,    na tom se vydělá.
+
+   C                 /H         /A        /H        C     /H  /A /H
+4. Mávl jsem rukou z okna, a ta vlaštovka doopravdy letěla,
+          C              /H             /A      /H      Csus2
+   i když bralo ji to na stranu, asi ji tížilo, že jaro nedělá ...
