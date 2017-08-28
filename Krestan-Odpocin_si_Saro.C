8f5b2fb8748905fae137a182c3a208c2d54cfdd6
Filip Krska <filip.krska@pirati.cz>
2017-08-28 17:25:26 +0200

    Odpocin_si_Saro

---
+++ Krestan-Odpocin_si_Saro.C
@@ -0,0 +1,47 @@
+Druhá tráva - Odpočin si, Sáro
+                    https://www.youtube.com/watch?v=86fCh69xfhk
+Orig: Eb - Kapo 8 - Kapo: 3
+
+I:
+   C        Ami  F  Emi  D  Cadd2
+
+       Ami  /A  /G /A F    C   /G  /Fum /E
+1. Odpočiň si,           Sáro,
+         Ami  /C /H  /A   Emi        Dmi /F /E /F /G /A /C /G
+   obrať list             a vzdej se mi,
+       Dmi     /G /F /G      Ami  /C /H  /A G
+   pro všechno          na zemi
+           Ami   /C /A  Fadd2 F  Cadd2
+   u tvých nohou,       Sá       ro.
+
+       Ami   F   C         Ami    Emi    Dmi
+2. Pamatuješ,  Sáro, tehdy v létě   v obilí
+        Dmi        Ami  Emi      Ami   Fadd2 F  Cadd2  G G G
+   jsme spolu kouřili        cigaretu, Sá       ro.
+
+R:
+         Dmi                                G6    C     G      Dmi
+   V tom pšeničném poli, kde ležet tak bolí, jsme spolu    leželi,
+        G                                                Dmi   Ami
+   v té pěně všech věcí, v té smyslné kleci, v tu slavnou  neděli,
+      F          C       Dmi       Ami        Ami     F    C
+   my smělí nesmělí jsme spolu neměli nic než retko,    Sáro.
+
+I:
+   Emi7   Ami   F  C     Ami       Emi    Dmi      F   Ami     F C
+
+         Ami      F   C        Ami   Emi      Dmi
+3. Je to k smíchu,  Sáro, jako tehdy   v pšenici,
+         Dmi7         Ami           Ami    F     C   G
+         pohleď v levici,   mám dvě sparty,    Sáro.
+R:
+       Dmi          G6(12)_F6                  C     G       Dmi
+   Ten nebohý kdosi,   kdo na zádech nosí tvou lásku  a tvůj klid
+       G                                           Dmi     Ami
+   ten zocelen z boje, pak příkoří tvoje snad musí     odčinit,
+   F               C        Dmi              Ami      Ami   F   C
+   kněžno, ztrácím nit, můj Bože, chtěl bych mít jeho záda,   Sáro
+
+O:
+   Ami      F C     Ami   Emi  Dmi
+   Dmi       Ami    Ami   F  C   Ami   F  C    Ami Emi Dmi...
