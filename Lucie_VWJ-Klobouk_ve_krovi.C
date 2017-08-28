f17236d373c845f02a0265ba7b4fce18e0c2edff
Filip Krska <filip.krska@pirati.cz>
2017-08-28 17:31:36 +0200

    Klobouk_ve_krovi

---
+++ Lucie_VWJ-Klobouk_ve_krovi.C
@@ -0,0 +1,36 @@
+VWJ, Lucie - Klobouk ve křoví
+                      https://youtu.be/XNeeq8y8V6I
+Orig: C
+
+I:
+   C    Fmi  C      C    Fmi  G
+
+   C    B9   C       C        B9      C
+1. Vítr vane pouští  po písku žene klobouk
+   Fmi    G7    C       Fmi     G7       C
+   zahnal ho do houští  starý a černý klobouk
+
+   C      B9    C      C        B9       C
+2. Kdepak je ta hlava, která klobouk nosila
+   Fmi     G7    C  C7   Fmi   G+      C
+   Byla černá či plavá?  Komu asi patřila?
+
+I: (whistle)
+   C      Fmi   C      C      Fmi   C
+   Fmi    G7    C      Fmi    G7    C  G
+
+   C      B9       C        C     B9    C
+3. Kdo to v poušti zmizel?  Odkud šel a kam?
+   Fmi  G7     C      Ami Fmi    G7       C   Fmi
+   Jaký to měl svízel,    že byl v poušti sám?
+
+I:
+   C  Fmi  C      C   Fmi  G
+
+   C     B9   C     C        B9         C  C7
+4. Jen zaváté stopy starý klobouk ve křoví
+   Fmi   G7      C     Fmi      G7      C     Fmi
+   nikdo nic nepochopí nikdo se nic nedoví
+
+O:
+   [:Xx C  Fmi :]
