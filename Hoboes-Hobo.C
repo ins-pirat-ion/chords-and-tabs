c4ccb5014e6fe78ce0d891bea1b2011cedea0339
Filip Krska <filip.krska@pirati.cz>
2017-09-25 17:30:06 +0200

    Hobo

---
+++ Hoboes-Hobo.C
@@ -0,0 +1,54 @@
+Hoboes - Hobo
+                  https://youtu.be/SlzojUqvmCQ
+Orig: C
+
+G+                  C                Emi          Dmi    G6      C  G+
+1.   Já nechci bejt sám, když koleje duní a temná noc do dáli ubíhá,
+             C                         Emi            Dmi     G7            C
+   co z toho k sakru mám, že znám plno vůní dalekejch cest, pohledy v očích mám
+                 F                  Ami            F                    G    G+
+   Tak co z toho mám že v srdci mám touhu jenom se mrknout za nejbližší strouhu
+            C                    Emi           Dmi       G6          C
+   já často šněroval svý toulavý boty, musí to bejt, můj vlak má zelenou.
+
+R:
+   C                  B     G7    C                   B    G7
+   Tmou nákladní vlak těžce duní, v mejch uších jenom vítr šumí,
+   C                F    C             G  G+       C              B      G7
+   kraj kolem trati spí, jen mašinfíra bdí.        Má ruku na rychlostní páce,
+   C             B    G7    C                F      C      Ab      G7    C   G+
+   má topič plný ruce práce já si na nápravě zpívám píseň, v které vlaky hřmí.
+
+               C                Emi            Dmi      G6        C  G+
+2. Když vagony hřmí a brzdař tě honí, necejtíš chlad, nohy tě nezebou.
+             C                          Emi             Dmi   G7           C
+   Jen běžíš po střeše a chtěl bys bejt pod ní, píšťala řve a tunel před tebou.
+                 F                 Ami                F                 G   G+
+   Tak co z toho mám, že znám celý státy, když nesmím na voči vlastního táty,
+               C                       Emi            Dmi       G6          C
+   když stromy rozkvetou, já stojím na trati, musí to bejt, můj vlak má zelenou
+
+R:
+   C                  B     G7    C                   B    G7
+   Tmou nákladní vlak těžce duní, v mejch uších jenom vítr šumí,
+   C                F    C             G  G+     C              B      G7
+   kraj kolem trati spí, jen mašinfíra bdí.      Má ruku na rychlostní páce,
+   C             B    G7    C                F      C      Ab      G7    C   G+
+   má topič plný ruce práce já si na nápravě zpívám píseň, v které vlaky hřmí.
+
+              C                Emi                Dmi    G6         C  G+
+3. Můj osobní vůz je vagon s vepřovým, můj rodnej dům je bouda brzdaře,
+                   C                  Emi           Dmi        G7            C
+   kde nechal jsem squaw, to vám už nepovím snad ve Frisku tam jsem byl na jaře
+                 F                 Ami               F              G      G+
+   Tak co z toho mám, pořád se jen toulat, v špinavý ruce pár centů žmoulat,
+             C                   Emi             Dmi       G6          C
+   jednou se netrefím a budou mě sbírat, musí to bejt, můj vlak má zelenou.
+
+R:
+   C                  B     G7    C                   B    G7
+   Tmou nákladní vlak těžce duní, v mejch uších jenom vítr šumí,
+   C                F    C             G  G+      C              B      G7
+   kraj kolem trati spí, jen mašinfíra bdí.       Má ruku na rychlostní páce,
+C             B    G7    C                F      C      Ab      G7    C B C B C
+má topič plný ruce práce já si na nápravě zpívám píseň, v které vlaky hřmí.
