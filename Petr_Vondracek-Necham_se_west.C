02620eaf1b0fc6dd1d59f022089671d0a881d2c7
Filip Krska <fkrska@pepper.brq.redhat.com>
2017-11-09 18:34:19 +0100

    Zafukane, Necham_se_west, Az_mi_bude_20_let_divce

---
+++ Petr_Vondracek-Necham_se_west.C
@@ -0,0 +1,52 @@
+Petr Vondráček - Nechám se west
+                      https://www.youtube.com/watch?v=F5bkiCUAoNg
+     Orig: E
+C Cadd2 C Cadd2 C                           Dmi7           G
+                Našel jsem si klidnej kout, v údolí pod skálou,
+         C                F G  C
+jen hlas ptáků a pár much.     Nechci se už odsud hnout,
+  D                F                          C
+a s hlavou ospalou,   si nechám zdát, že jsem vzduch,
+       Fmaj          Ami7          D7       
+kterej dýcháš nocí i dnem, že jsem stín v tvých patách
+               Fmaj   G              C
+          a ty pomalu kráčíš mým snem.
+R:
+   C          Bmaj           F                       C
+   Já vím, že dlouhou půjdem tmou, než naše cesty se zkříží.
+             Gmi7            F                        C
+snad smím je ve snách předběhnout, a doufat, že už se blíží,
+    Dmi7            Ami7       F
+ten čas, kdy uvěříš v nás, kdy oči se střetnou,
+G                 C
+po těle přejde ti mráz.
+           Bmaj         F                        C
+Já vím, že dlouho už tě znám, a přitom je to jen chvíle.
+          Gmi7            F                      C
+Vrátím se zpátky, když to vzdám, anebo zbývá jen míle.
+                  Dmi7  Ami7            G         C
+          Prachem cest,          k tobě nechám se vést.
+I:
+     C     Fmaj   Dmi7  C   C   D7   F   C
+     
+C           Fmaj          Ami7      D7
+Chci v tobě dýchat nocí i dnem, být stín v tvých patách,
+                   Fmaj   G          C
+          když ty, pomalu kráčíš mým snem.
+R:
+   C          Bmaj           F                       C
+   Já vím, že dlouhou půjdem tmou, než naše cesty se zkříží.
+             Gmi7            F                        C
+Snad smím je ve snách předběhnout, a doufat, že už se blíží,
+    Dmi7            Ami7       Fmaj
+ten čas, kdy uvěříš v nás, kdy oči se střetnou,
+G                 C
+po těle přejde ti mráz.
+           Bmaj         F                        C
+Já vím, že dlouho už tě znám, a přitom je to jen chvíle.
+          Gmi7            F                      C
+Vrátím se zpátky, když to vzdám, anebo zbývá jen míle.
+        Dmi7  Ami7            G         C         F    Cadd2
+Prachem cest,          k tobě nechám se vést.
+       F      Gmi7     C
+K tobě nechám       se vést
