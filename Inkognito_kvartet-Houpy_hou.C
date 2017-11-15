5187753612672ec3d1555a72c1f7317d80bdcc10
Filip Krska <fkrska@pepper.brq.redhat.com>
2017-11-15 13:14:22 +0100

    Houpy_hou, Balada_Chanuka, Dnes_se_mi_o_tobe_zdalo

---
+++ Inkognito_kvartet-Houpy_hou.C
@@ -0,0 +1,36 @@
+Inkognito kvartet - Houpy hou
+                      https://youtu.be/wHlHkkaBSsY
+Orig: D - Kapo 2
+
+I:
+    C6   G76   C6   G76
+
+C6              Emi7 Ami7            E+
+Hučí řeka, hučí tmou píseň svoji dumavou,
+Dmi7             G7            C    G+
+proč jsi má milá hlavu zmámila mou.
+C6                Emi7 Emi               G+
+Čekám na tě děvče mé,  v širou dáli půjdeme,
+Ami7            D7                G6
+nemusíš se bát, srdce chci ti dát své houpyhoupyhou
+
+   C          H7         Dmi               G7
+1. Houpy-hou, houpy-hou, na vlnách se keňu houpá,
+   Dmi                 G7      C6       G+
+   píseň má ku hvězdám stoupá, houpy-hou.
+
+   C          H7      Dmi             G7
+2. A já s mou kytarou jedeme tak nocí sami,
+   Dmi               G7     C     C7
+   až nás píseň řeky zmámí, hou.
+
+R:
+   C9       C76   C9         C76
+   Děvče mé bílé, sen o tobě snívám,
+   D7          D9       Dmi        G7
+   do vln když smutných v podvečer sedívám
+
+   C         H7     Dmi                G7
+3. Kde je má osada, kde je štěstí, kamarádi,
+   Dmi              G7     C6         Cmaj
+   dálka moje srdce svádí, houpy-hou.
---
+++ Plihal-Dnes_se_mi_o_tobe_zdalo.C
@@ -0,0 +1,39 @@
+Karel Plíhal - Dnes se mi o tobě zdálo
+
+               https://youtu.be/ErMmJ659uIg
+Orig: G# - Kapo: 8
+
+   C           Dmi7   Emi    C7   
+1. Dnes se mi  o Tobě zdálo, 
+  Dmi        Dmi75b     C     /B  A7
+   tak jsem  se do děje vložil.
+  Dmi         Fmi      Emi7  A7  /H  /C
+   Jenže šest hodin je málo,
+  Dmi7      D7            G9 G+
+   abych to co bych chtěl prožil.
+
+  C           Dmi7   Emi   C  /H  /A
+2. Dnes se mi o Tobě zdálo, 
+  Dmi7     Fmi          C A7
+   dalas mi   vše co se dává.
+  Dmi          Fmi         Emi   A7
+   Vzal jsem si,  a tak se stalo, 
+  Dmi7      D7     G9  G+
+   že se mi šíleně vstává.
+
+R:
+  C                   Ami7          A6   A+  A
+   K ránu sen bledne, trhá se loupá na kusy.
+D7          Dmi7  D7                    G9 Emi D#mi Dmi
+Proč jen si ve dne přehnaně skoupá na pusy.
+
+   C        Dmi7        Emi    C7   
+3. Čas letí    a sny se zdají 
+  Dmi        Dmi75b       C     /B  A7
+   vyrůstáš    z naivních rolí
+  Dmi         Fmi       Emi7  A7  /H  /C
+   Ti co sny  nemaj` se mají
+  Dmi7      D7      G9   G+
+   protože zklamání bolí
+       Cmaj
+   Ach jo...
