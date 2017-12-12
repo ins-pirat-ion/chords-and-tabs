1384445e07a08e284e36f1f14157511e43d53686
Filip Krska <fill_io@centrum.cz>
2017-12-12 14:10:18 +0100

    Hej_pane_deda_Mraz

---
+++ Vlasta_Redl-Hej_pane_deda_Mraz.C
@@ -0,0 +1,49 @@
+Vlasta Redl - Hej pane děda Mráz
+                                https://youtu.be/zdfJDUf75-o
+     Orig: C
+I:
+   C    Ami7    Dmi7 Emi D/F#   G    C
+   C                    F               Emi
+1. Hej, pane Děda Mráz, to jste se snad splet',
+   F       Emi  Dmi C     G     Ami7 D/F# G
+   už jste zase naděloval u špatné   ho   zvonku,
+             Hmi75- C    D/F#  G
+   u souseda stojí  nový chevrolet,
+   C         Ami7    Dmi7 Emi D/F#   G     C  (I:)
+   a má milá dostala za   se  jenom  spo onku.
+
+   C                    F            Emi
+2. Hej, pane Děda Mráz, zase jste to splet',
+   F         Emi  Dmi      C   G   Ami7 D/F# G
+   ptal jsem se i známých, ale dopadli  jsme stejně,
+                   Hmi75- C      D/F#  G
+   a přitom už vám píšem  lístky kolik let,
+   C              Ami7     Dmi7 Emi D/F# G    C  (I:)
+   stejně vždycky dostanem to,  co  oby  če ejně.
+
+   C                        F           Emi
+3. Hej, pane Děda Mráz, kdy přijdu na řadu,
+   F          Emi   Dmi  C     G     Ami7 D/F# G
+   vždyť jsem přece nezlobil - zeptejte   se   v práci,
+            Hmi75-     C     D/F# G
+   asi máte v počítači někde záva du
+             C         Ami7     Dmi7 Emi D/F#    G     C  (I:)
+             a ty naše lístečky se   tam všecky  ztrá ací.
+R:
+   Dmi       G          /D   /G     C
+   Jen se mi zdá, že se pletete moc často,
+   G            C7       Gmi7          F
+   těm za deset tisíc, a tobě, Vlasto, za sto,
+   Dmi            G7          C     Dmi7 G
+   snad bychom se spolu nějak třeba dohodnuli,
+   C                F   C  Dmi7            G   C  (I:)
+   vždyť ten rozdíl nedělá nic víc než dvě nu uly.
+
+   C                    F            Emi
+4. Hej, pane Děda Mráz, zase jste to splet',
+       F     Emi  Dmi     C     G    Ami7 D/F# G
+   ale za ty roky jsem si na to už doce   la   zvykl,
+               Hmi75-          C    D/F# G
+   v duchu vás omlouvám, to už umím nazpaměť,
+   C                Ami7          Dmi7 Emi D/F# G   C  (I:)
+   ale dejte pozor, aby vám někdo py   tel neu  fí ikl
