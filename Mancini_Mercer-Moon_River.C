22d9b05b54c5c01a46615b2a7ce402c2f9d2c2df
Filip Krska <fill_io@centrum.cz>
2018-01-09 22:55:34 +0100

    Moon_River

---
+++ Mancini_Mercer-Moon_River.C
@@ -0,0 +1,27 @@
+Henry Mancini, Mercer - Moon River
+
+https://youtu.be/Q7SI7N22k_A
+
+Orig: F# - Kapo: 6
+
+C C C    Ami   F            C/E
+    Moon river wider than a mile,
+    F               C/E        Hmi75- E7
+I'm crossing you in style some day
+Ddim Ami7  C7         F     B95-
+old  dream maker, you heart breaker,
+    Ami  Ami7   D9/F# Emi7 A7    G7
+wherever you're going I'm  going your way
+
+C   Ami        F              C/E
+Two drifters,  off to see the world,
+        F             C/E      Hmi75- E7
+There's such a lot of world to see
+      Ami Ami7    D9/F#  F7          C
+Where af  ter the same     rainbow's end
+F                   C/E
+  waiting round the bend,
+F                  C/E
+   my Huckleberry  friend,
+Ami     Dmi    G7 G6     F  /E  /D  C
+Moon    River        and me 
