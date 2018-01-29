ff5aea8a339794ec0b11afa23a2da23515692807
Filip Krska <fill_io@centrum.cz>
2018-01-29 13:40:11 +0100

    Hruska, The_beat_goes_on

---
+++ Sonny_Bono-The_beat_goes_on.C
@@ -0,0 +1,41 @@
+Sonny Bono - The Beat Goes On
+                             https://youtu.be/Zblr8g3P7tw
+Orig: F - Kapo: 5
+
+  C76   G79+ C76    G79+ (and on and on)
+♪ C B   G B  C   B  G B  G
+
+n'the beat goes on, the beat goes on
+                Drums keep pounding a rhythm to the brain
+        Ladidadidi, ladidadida
+♪                                [: E E  DE  F  E  DE :]
+    Charleston was once the rage, a ha
+    History has burned that stage,  a haa
+The mini skirts, the current thing, a ha
+    Teenybopper is our newborn king, a ha
+
+♪            [: C B   G B  C   B   G    B  :] 
+n'the beat goes on,            the beat goes on
+                Drums keep pounding a rhythm to the brain
+        Ladidadidi, ladidadida
+
+ Grocery store's a super mart, a ha
+ Little girls still break their hearts, a ha
+ Men still keep on marching off to war
+Electrically they keep a baseball score
+
+n'the beat goes on, the beat goes on
+                Drums keep pounding a rhythm to the brain
+        Ladidadidi, ladidadida
+I:
+(copy the melody)
+
+    Grandmas sit in chairs and reminisce
+    Boys keep chasing girls to get a kiss
+The cars keep going faster all the time
+    Bums still cry "hey buddy, have you got a dime"
+
+n'the beat goes on, the beat goes on
+                Drums keep pounding a rhythm to the brain
+        Ladidadidi, ladidadida
+n'the beat goes on, the beat goes on
