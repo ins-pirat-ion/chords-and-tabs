687ee3389d6d9da2e5cee02e4ba7b43332364564
Filip Krska <fill_io@centrum.cz>
2018-05-18 15:48:38 +0200

    The_Girl_From_Ipanema

---
+++ Gilberto-The_Girl_From_Ipanema.C
@@ -0,0 +1,46 @@
+Astrud Gilberto, João Gilberto, Stan Getz - The Girl from Ipanema
+                                     https://youtu.be/c5QfXjsoNe4
+Orig: C# - Kapo: 1
+
+I:
+   Cmaj
+   Dindondon bringongon bringongon
+
+Cmaj                              D7
+Tall'n'tan and young'n'lovely the girl from Ipanema goes walking
+    Dmi7                  Ddim    G7           Cmaj
+And when she passes, each one she passes goes     "Ahhh."
+
+Cmaj
+When she walks, she's like a samba
+     D7
+That swing's so cool and sway's so gentle,
+          Dmi7                  Ddim    G7           Cmaj
+     That when she passes, each one she passes goes   "Ahhh."
+R:
+   C#maj                 F#75-   G#mi7       C#mi75-    A9
+   Oh but I watch her so sadly   How   can I tell her I love her
+   D9           Dmi7          B9
+   Yes, I would give my heart gladly,
+            Emi7                      A75-
+   But each day when she walks to the sea,
+          Dmi7                         C#9
+      She looks straight ahead, not at me.
+I:
+        [3x  Cmaj      D7      Dmi7   Ddim  G7 Cmaj :]
+R:
+   C#maj                 F#75-   G#mi7       C#mi75-    A9
+   Oh but I watch her so sadly   How   can I tell her I love her
+   D9           Dmi7          B9
+   Yes, I would give my heart gladly,
+            Emi7                      A75-
+   But each day when she walks to the sea,
+       Dmi7                         C#9
+   She looks straight ahead, not at me.
+
+Cmaj                              D7
+Tall'n'tan and young'n'lovely the girl from Ipanema goes walking
+    Dmi7                Ddim       G7          Cmaj
+And when she passes,  I smile, But she doesn't see,
+   C#9         Cmaj    C#9         Cmaj
+no she doesn't see, No she doesn't see.
