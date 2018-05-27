a80346010c6d7aa171efa53b101bda99cc00e898
Filip Krska <fill_io@centrum.cz>
2018-05-27 17:14:10 +0200

    Mary_Lou

---
+++ Nezmari-Mary_Lou.C
@@ -0,0 +1,49 @@
+Nezmaři - Mary Lou 
+                      https://youtu.be/bHAxATu7QpI
+Orig: D - Kapo: 2
+
+1.
+C (slow)    Fmaj    (silent)  C                            Emi7
+ Slyšíš, co zvon zpívá malá Mary Lu slyšíš co zvon zpívá malá Mary Lu
+                Fmaj G      Ami        C           Ami
+           jeho tóny sladce zní, zpívá píseň svatební
+           F               Fmaj     F6      C (bluegrass) F6
+           slyšíš, co zvon zpívá, malá Mary Lou.
+2.
+    C          Fmaj    F6      C                          Emi7
+Tvá matka slzy stírá malá Mary Lou tvá matka slzy stírá malá Mary Lou
+               Fmaj G        Ami            C           Ami
+           tvý šaty jsou jak sníh, jak jsou hebké na dlaních
+               Fmaj                Dmi7    C  F6
+           tvá matka slzy stírá, malá Mary Lou.
+3.
+C            Fmaj   F6      C                        Emi7
+Ženich vozem jede malá Mary Lou, ženich vozem jede malá Mary Lou
+           Fmaj    G    Ami        C           Ami
+           z plání vřesových už je slyšet jeho smích
+           Fmaj         /A      F6      C
+           ženich vozem jede, malá Mary Lou.
+4.
+                 Fmaj    F6     C                           Emi7
+Proč najednou je ticho malá MaryLu proč najednou je ticho malá MaryLu
+                 Fmaj   G      Ami    C          Ami
+           co se stalo, kdo to ví, na pláni vřesový
+                Fmaj        /E       /D      C  F6
+           proč najednou je ticho, malá Mary Lou.
+5.
+C             Fmaj     F6      C                          Emi7
+Tam za velkou skálou malá Mary Lou tam za velkou skálou malá Mary Lou
+               Fmaj  G    Ami        C               Ami
+           vůz najel na kámen, Billy spad' na černou zem
+           Fmaj                    Dmi7    C  F6
+           tam za velkou skálou, malá Mary Lou.
+6.
+C               Fmaj    F6      C                          Emi7
+Slyšíš, co zvon zpívá malá Mary Lu slyšíš, co zvon zpívá malá Mary Lu
+                Fmaj G     Ami        C           Ami
+           jeho tóny hořce zní, zpívá píseň pohřební
+           Fmaj                     Dmi7    C  /H /A /G
+           slyšíš, co zvon zpívá, malá Mary Lou.
+O:
+    [: C /H /A /G  :]  Cmaj
+    hůůůůů
